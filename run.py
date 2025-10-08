557a7ebf37f73f54de6b9bc316e8e2da2259b336
scude <4232912+scude@users.noreply.github.com>
2025-10-08 14:54:28 +0200

    feat(app): Add conf for heroku cloud

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 """Entry point for running the Flask development server."""
 from __future__ import annotations
 
-from app import create_app
-from app.config import MAX_CONTENT_LENGTH
-
-app = create_app({"MAX_CONTENT_LENGTH": MAX_CONTENT_LENGTH})
-
+from app import app
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
