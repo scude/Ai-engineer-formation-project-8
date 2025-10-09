2da555ce288dd67cff1ce53e84a8f0ca31358c6c
scude <4232912+scude@users.noreply.github.com>
2025-10-09 11:33:30 +0200

    Revert "feat(app): Add conf for heroku cloud"
    This reverts commit 557a7ebf37f73f54de6b9bc316e8e2da2259b336.

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 """Entry point for running the Flask development server."""
 from __future__ import annotations
 
-from app import app
+from app import create_app
+from app.config import MAX_CONTENT_LENGTH
+
+app = create_app({"MAX_CONTENT_LENGTH": MAX_CONTENT_LENGTH})
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
